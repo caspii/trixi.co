dc8fb526fa908bb9139f6584864ff947e6da8fb0
caspii <caspar.wrede@gmail.com>
2017-03-17 16:47:37 +0100

    Fixed indentation

--- static/custom.js
+++ static/custom.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-  // Init all tooltips and popovers
-  $('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="popover"]').popover();
-  new Clipboard('.clip-button');
+    // Init all tooltips and popovers
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+    new Clipboard('.clip-button');
 
-  $(".person-button").click(function(){
+    $(".person-button").click(function(){
         var button = $(this);
         var field = '#person_count'
         var old_value = parseInt($(field).val())
         if (button.text() == '+') {
-          new_value = old_value + 1
-          if (new_value <=10) {
-            $(field).val(new_value)
-          }
+                new_value = old_value + 1
+            if (new_value <=10) {
+                $(field).val(new_value)
+            }
         } else {
-          new_value = old_value - 1
-          if (new_value >=1) {
-            $(field).val(new_value)
-          }
+            new_value = old_value - 1
+            if (new_value >=1) {
+                $(field).val(new_value)
         }
+    }
     });
 
 });
