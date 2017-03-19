fa42701195c0920905bcf5bd801ada4da62f8919
caspii <caspar.wrede@gmail.com>
2017-03-19 23:36:50 +0100

    Fixed yet another crash when no cookie present

--- session_manager.py
+++ session_manager.py
@@ -4,6 +4,7 @@ import logging
 
 
 def store_user(request, response, project_key, user_id):
+    print "Storing user"
     projects = {}
     json_str = request.cookies.get('projects')
     if json_str is not None:
@@ -17,14 +18,18 @@ def store_user(request, response, project_key, user_id):
     response.set_cookie('projects', cookie_json, max_age=10 * 365 * 24 * 60 * 60)
 
 
+
 def get_user(request, project_key):
     """Return the ID of the current user for the given project"""
-    projects = {}
+    projects = request.cookies.get('projects')
+    if projects is None:
+        return None
     try:
-        projects = json.loads(request.cookies.get('projects'))
+        projects = json.loads(projects)
     except (ValueError, KeyError, TypeError):
         print "JSON format error"
         logging.exception("Cookie json could not be decoded")
+        return None
     user_id = projects.get(project_key)
     if user_id is not None:
         return int(user_id)
