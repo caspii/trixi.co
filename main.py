6695304b9e6f46cd31e9f46eb70e90603ae31081
caspii <caspar.wrede@gmail.com>
2017-04-27 22:53:21 +0200

    Tweaked project created flash

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def people():
             return render_template('people.html', form=form)
     elif request.method == 'POST':
         if form.validate():
-            flash("Whoop! You created a project")
+            flash("Nice! You created a project. Please bookmark this page.")
             project_name = session['project_name']
             people_names = [p.data for p in form.people]
             project_key = Project.new(project_name, people_names)
